32a0d74e627011c7c26419f0921c3c068bd53249
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-07-22 19:12:21 -0500

    updated colorschema colors

--- dot_config/nvim/lua/alex/plugins/colorscheme.lua
+++ dot_config/nvim/lua/alex/plugins/colorscheme.lua
@@ -2,7 +2,14 @@ return {
 	"loctvl842/monokai-pro.nvim",
 	priority = 1000,
 	config = function()
-		require("monokai-pro").setup()
+		require("monokai-pro").setup({
+			overridePalette = function(filter)
+				return {
+					background = "#1A1B26",
+					text = "#f2f2f2",
+				}
+			end,
+		})
 		vim.cmd("colorscheme monokai-pro")
 	end,
 }
