9663c05f37236a5f5e94725af5889365cb230131
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-07-29 19:58:59 -0500

    added folding

---
+++ dot_config/nvim/lua/alex/plugins/folding.lua
@@ -0,0 +1,24 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "test" })
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+		-- local winid = require("ufo").peekFoldedLinesUnderCursor()
+		-- if not winid then
+		-- 	vim.lsp.buf.hover()
+		-- end
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+	end,
+}
