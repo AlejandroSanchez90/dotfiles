e3020070b90be05d06d496f2432729adedd8d69c
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-08-02 11:34:05 -0500

    fixed noice error

--- dot_config/nvim/lua/alex/plugins/noice.lua
+++ dot_config/nvim/lua/alex/plugins/noice.lua
@@ -1,19 +1,17 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
 	dependencies = {
-
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
 	config = function()
+		require("notify").setup({
+			timeout = 1000,
+			background_colour = "#000000",
+			max_width = 50,
+		})
+
 		require("noice").setup({
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
