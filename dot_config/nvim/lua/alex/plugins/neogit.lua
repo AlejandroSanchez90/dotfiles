8f35c6dceb368e344403241fcb97523b7002ab81
Alejandro Sanchez <asanchez3@houasanchez3-mbQ659.eogresources.com>
2024-08-24 13:39:51 -0500

    neogit keymaps fix

--- dot_config/nvim/lua/alex/plugins/neogit.lua
+++ dot_config/nvim/lua/alex/plugins/neogit.lua
@@ -14,8 +14,10 @@ return {
 		local keymap = vim.keymap -- for conciseness
 		keymap.set("n", "<leader>go", "<cmd>Neogit<cr>", { desc = "Open NeoGit" })
 		keymap.set("n", "<leader>gf", "<cmd>Neogit fetch<cr>", { desc = "Fetch" })
-		keymap.set("n", "<leader>gg", "<cmd>Neogit branch<cr>", { desc = "Commit" })
+		keymap.set("n", "<leader>gb", "<cmd>Neogit branch<cr>", { desc = "Commit" })
+		keymap.set("n", "<leader>gg", "<cmd>Neogit commit<cr>", { desc = "Commit" })
 		keymap.set("n", "<leader>gp", "<cmd>Neogit push<cr>", { desc = "Push" })
+		keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "Status" })
 
 		keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory % --base=LOCAL<cr>", { desc = "File github history" })
 
