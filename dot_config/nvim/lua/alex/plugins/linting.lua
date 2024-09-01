fc84fac177ef9039a9a41f05439bd30ea1b3f73d
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-09-01 16:27:06 -0500

    updated tmux and linting

--- dot_config/nvim/lua/alex/plugins/linting.lua
+++ dot_config/nvim/lua/alex/plugins/linting.lua
@@ -1,41 +1,29 @@
 return {
-	"dense-analysis/ale",
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local g = vim.g
+		local lint = require("lint")
 
-		-- Enable linting for ESLint and TSLint
-		g.ale_linters = {
-			javascript = { "eslint" },
-			javascriptreact = { "eslint" },
-			typescript = { "eslint", "tslint" },
-			typescriptreact = { "eslint", "tslint" },
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
 		}
 
-		-- Enable fixing of issues automatically (optional)
-		g.ale_fix_on_save = 1
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		-- Use specific linters for various file types
-		g.ale_javascript_eslint_executable = "eslint"
-		g.ale_javascript_eslint_options = "-c .eslintrc.js"
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 
-		g.ale_typescript_eslint_executable = "eslint"
-		g.ale_typescript_eslint_options = "-c .eslintrc.js"
-
-		g.ale_typescript_tslint_executable = "tslint"
-		g.ale_typescript_tslint_options = "-c tslint.json"
-
-		-- Optionally, enable auto-fix for eslint
-		g.ale_fixers = {
-			javascript = { "eslint" },
-			javascriptreact = { "eslint" },
-			typescript = { "eslint" },
-			typescriptreact = { "eslint" },
-		}
-
-		-- Enable linting on text changes
-		g.ale_lint_on_text_changed = "always"
-
-		-- Use quickfix list instead of location list
-		g.ale_set_quickfix = 1
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
 	end,
 }
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Medium" })
 config.font_size = 16
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
+config.window_close_confirmation = "NeverPropmt"
 
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
