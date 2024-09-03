00d26a4e9d75b41719a5b87cfac84b4b2f0cc564
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-09-03 08:19:17 -0500

    wezterm typo

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,7 +5,7 @@ config.font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Medium" })
 config.font_size = 16
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
-config.window_close_confirmation = "NeverPropmt"
+config.window_close_confirmation = "NeverPrompt"
 
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
