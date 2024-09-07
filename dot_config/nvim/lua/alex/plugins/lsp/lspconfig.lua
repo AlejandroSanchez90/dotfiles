1fad1d9fbe490fda5be5893c97e30f0605307d02
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-09-06 19:15:26 -0500

    hacky fix for tsserver deprecated options

--- dot_config/nvim/lua/alex/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/alex/plugins/lsp/lspconfig.lua
@@ -82,13 +82,15 @@ return {
 		mason_lspconfig.setup_handlers({
 			-- default handler for installed servers
 			function(server_name)
+				-- TODO: This is a hack to fix the server_name, but it should be fixed in mason-lspconfig
+				server_name = server_name == "tsserver" and "ts_ls" or server_name
 				lspconfig[server_name].setup({
 					capabilities = capabilities,
 				})
 			end,
 
-			["tsserver"] = function()
-				lspconfig["tsserver"].setup({
+			["ts_ls"] = function()
+				lspconfig["ts_ls"].setup({
 					capabilities = capabilities,
 					filetypes = {
 						"javascriptreact",
