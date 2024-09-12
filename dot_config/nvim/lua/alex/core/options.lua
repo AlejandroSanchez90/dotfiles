429dacb32887c8f6852667afdb01056dca39d001
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-09-12 14:22:44 -0500

    disable auto comment on new line

--- dot_config/nvim/lua/alex/core/options.lua
+++ dot_config/nvim/lua/alex/core/options.lua
@@ -64,3 +64,11 @@ autocmd("TextYankPost", {
 		})
 	end,
 })
+
+-- disable comment on new line
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
+})
