1fe6c095bec17a378a559371a96dc8178a39693d
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2025-05-12 09:03:40 -0500

    enhance plugins and keybindings

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,16 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
+wezterm.log_error("Version " .. wezterm.version)
 config.font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Medium" })
 config.font_size = 16
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
 config.window_close_confirmation = "NeverPrompt"
 
-config.window_background_opacity = 0.85
-config.max_fps = 240
+config.window_background_opacity = 0.80
+config.max_fps = 120
 
-config.macos_window_background_blur = 6
+config.macos_window_background_blur = 8
 config.win32_system_backdrop = "Acrylic"
 
 config.keys = {
