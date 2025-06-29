7400c70fb87deb1772a8d2a3b0b350ab380d1bf8
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-06-29 09:24:42 -0500

    grep diff against head

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -40,7 +40,7 @@ return {
               f_opts,
               {
                 cmd = 'git',
-                args = { 'diff', '--unified=0' },
+                args = { 'diff', 'HEAD', '--unified=0' },
                 transform = function(item) ---@param item snacks.picker.finder.Item
                   local line = item.text
                   -- [[Header]]
@@ -83,7 +83,6 @@ return {
         },
         smart = {
           multi = { 'buffers', 'recent', 'files' },
-          -- sort = { fields = { 'source_id', 'idx' } },
           filter = { cwd = true },
         },
       },
