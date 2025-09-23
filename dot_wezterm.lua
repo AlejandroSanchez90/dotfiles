90e265537588f58d940306f74db874b9d7f9384c
Alejandro Sanchez <asanchez3@houasanchez3-mbQ659.eogresources.com>
2025-09-23 13:43:03 -0500

    eslint fix, more tokyonight updates

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,13 +4,14 @@ wezterm.log_error("Version " .. wezterm.version)
 config.font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Medium" })
 config.font_size = 16
 config.window_decorations = "RESIZE"
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Tokyo Night"
+
 config.window_close_confirmation = "NeverPrompt"
 
 config.window_background_opacity = 0.80
 config.max_fps = 120
 
-config.macos_window_background_blur = 8
+config.macos_window_background_blur = 20
 config.win32_system_backdrop = "Acrylic"
 
 config.keys = {
