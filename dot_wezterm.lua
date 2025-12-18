f25e1ad6da22d899dc65e6f78b01cf4684b669db
Alejandro Sanchez <asanchez3@houasanchez3-mbQ659.eogresources.com>
2025-12-18 08:52:58 -0600

    removed some chezmoi files

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 wezterm.log_error("Version " .. wezterm.version)
-config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium" })
+config.font = wezterm.font({ family = "JetBrainsMono Nerd Font" })
 config.font_size = 16
 config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night"
